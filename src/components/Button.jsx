c7d0373acb6b60311e6bc05fecd7fffc85d69018
silver-josh <88242593+silver-josh@users.noreply.github.com>
2023-07-01 00:17:29 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ styles }) => (
-  <a href="http://www.app.crowdverse.net" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  <a href="https://app.crowdverse.net" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
     Get Started
   </a>
 );
